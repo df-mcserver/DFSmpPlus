650588971461111330a2384c019d9f5a65ae3804
deadfry42 <pooface442@gmail.com>
2025-10-07 21:37:02 +0100

    imply vampire sword upgrade rather than explicit message

--- src/main/java/uk/co/nikodem/dFSmpPlus/Items/Metas/VampireSwordMeta.java
+++ src/main/java/uk/co/nikodem/dFSmpPlus/Items/Metas/VampireSwordMeta.java
@@ -1,10 +1,15 @@
 package uk.co.nikodem.dFSmpPlus.Items.Metas;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import uk.co.nikodem.dFSmpPlus.Constants.Keys;
 import uk.co.nikodem.dFSmpPlus.Items.DFItemUtils;
@@ -12,6 +17,9 @@ import uk.co.nikodem.dFSmpPlus.Items.DFMaterial;
 import uk.co.nikodem.dFSmpPlus.Items.DFMaterialMeta;
 
 import javax.annotation.Nullable;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VampireSwordMeta implements DFMaterialMeta {
     public final int MAX_STAGE = 9;
@@ -21,8 +29,10 @@ public class VampireSwordMeta implements DFMaterialMeta {
         if (entity instanceof Player target) {
             ItemStack item = plr.getInventory().getItemInMainHand();
             Integer stage = incrementStage(item);
+            if (stage == null) return;
+            playSnapSound(plr);
+            updateLore(item, stage);
             updateModel(item, stage);
-            plr.sendMessage("Vampire sword upgrade to stage "+stage);
         }
     };
 
@@ -58,5 +68,20 @@ public class VampireSwordMeta implements DFMaterialMeta {
     public void updateModel(ItemStack item, Integer stage) {
         if (stage == null) return;
         DFItemUtils.setModel(item, "vamp_stage"+stage);
+    }
+
+    public void playSnapSound(Player plr) {
+        plr.getWorld().playSound(plr, Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 0.3F, 1F);
+    }
+
+    public void updateLore(ItemStack item, Integer stage) {
+        if (item == null) return;
+        if (stage == null) return;
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return;
+        List<TextComponent> newLore = new ArrayList<>(DFMaterial.VampireSword.getLore());
+        newLore.add(Component.text("  +"+stage+" damage", NamedTextColor.RED));
+        meta.lore(newLore);
+        item.setItemMeta(meta);
     }
 }
