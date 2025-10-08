9f1d958d9eefbbd21e4adc56b9c17955d9299a3a
deadfry42 <pooface442@gmail.com>
2025-10-08 17:00:15 +0100

    add more chisel blocks

--- src/main/java/uk/co/nikodem/dFSmpPlus/Constants/ChiselBlocks.java
+++ src/main/java/uk/co/nikodem/dFSmpPlus/Constants/ChiselBlocks.java
@@ -8,6 +8,7 @@ import static java.util.Map.entry;
 
 public class ChiselBlocks {
     public final static Map<Material, Map.Entry<Material, Material>> blockMappings = Map.ofEntries(
+            // btw reference
             entry(Material.STONE, entry(Material.GRAVEL, Material.GRAVEL)),
             entry(Material.GRAVEL, entry(Material.PALE_OAK_LEAVES, Material.FLINT)),
 
@@ -28,7 +29,24 @@ public class ChiselBlocks {
             entry(Material.DEEPSLATE_COPPER_ORE, entry(Material.DEEPSLATE, Material.AIR)),
             entry(Material.DEEPSLATE_GOLD_ORE, entry(Material.DEEPSLATE, Material.AIR)),
             entry(Material.DEEPSLATE_DIAMOND_ORE, entry(Material.DEEPSLATE, Material.AIR)),
-            entry(Material.DEEPSLATE_LAPIS_ORE, entry(Material.DEEPSLATE, Material.AIR))
-            // TODO: add concrete -> concrete powder
+            entry(Material.DEEPSLATE_LAPIS_ORE, entry(Material.DEEPSLATE, Material.AIR)),
+
+            // actually useful
+            entry(Material.BLACK_CONCRETE, entry(Material.BLACK_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.BLUE_CONCRETE, entry(Material.BLUE_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.BROWN_CONCRETE, entry(Material.BROWN_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.CYAN_CONCRETE, entry(Material.CYAN_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.GRAY_CONCRETE, entry(Material.GRAY_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.GREEN_CONCRETE, entry(Material.GREEN_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.LIGHT_BLUE_CONCRETE, entry(Material.LIGHT_BLUE_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.LIGHT_GRAY_CONCRETE, entry(Material.LIGHT_GRAY_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.LIME_CONCRETE, entry(Material.LIME_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.MAGENTA_CONCRETE, entry(Material.MAGENTA_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.ORANGE_CONCRETE, entry(Material.ORANGE_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.PINK_CONCRETE, entry(Material.PINK_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.PURPLE_CONCRETE, entry(Material.PURPLE_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.RED_CONCRETE, entry(Material.RED_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.WHITE_CONCRETE, entry(Material.WHITE_CONCRETE_POWDER, Material.AIR)),
+            entry(Material.YELLOW_CONCRETE, entry(Material.YELLOW_CONCRETE_POWDER, Material.AIR))
     );
 }
