b0105f0f9eb10e2d3d36114616ab59626e3abde0
deadfry42 <pooface442@gmail.com>
2025-11-04 01:06:38 +0000

    test conditionals in ControlledShapelessRecipe

--- src/main/java/uk/co/nikodem/dFSmpPlus/Crafting/CustomRecipeMethods/SmithingTable/CustomItemRepresentation.java
+++ src/main/java/uk/co/nikodem/dFSmpPlus/Crafting/CustomRecipeMethods/SmithingTable/CustomItemRepresentation.java
@@ -26,6 +26,54 @@ public class CustomItemRepresentation {
         this.item = item.toItemStack();
     }
 
+    public CustomItemRepresentation(ItemStack item, boolean strict) {
+        this.item = item;
+        this.strict = strict;
+    }
+
+    public CustomItemRepresentation(Material item, boolean strict) {
+        this.item = new ItemStack(item);
+        this.strict = strict;
+    }
+
+    public CustomItemRepresentation(DFMaterial item, boolean strict) {
+        this.item = item.toItemStack();
+        this.strict = strict;
+    }
+
+    public CustomItemRepresentation(ItemStack item, Function<ItemStack, Boolean> check) {
+        this.item = item;
+        this.check = check;
+    }
+
+    public CustomItemRepresentation(Material item, Function<ItemStack, Boolean> check) {
+        this.item = new ItemStack(item);
+        this.check = check;
+    }
+
+    public CustomItemRepresentation(DFMaterial item, Function<ItemStack, Boolean> check) {
+        this.item = item.toItemStack();
+        this.check = check;
+    }
+
+    public CustomItemRepresentation(ItemStack item, Function<ItemStack, Boolean> check, boolean strict) {
+        this.item = item;
+        this.check = check;
+        this.strict = strict;
+    }
+
+    public CustomItemRepresentation(Material item, Function<ItemStack, Boolean> check, boolean strict) {
+        this.item = new ItemStack(item);
+        this.check = check;
+        this.strict = strict;
+    }
+
+    public CustomItemRepresentation(DFMaterial item, Function<ItemStack, Boolean> check, boolean strict) {
+        this.item = item.toItemStack();
+        this.check = check;
+        this.strict = strict;
+    }
+
     public CustomItemRepresentation setStrict(boolean strict) {
         this.strict = strict;
         return this;
--- src/main/java/uk/co/nikodem/dFSmpPlus/Crafting/Recipes/CompassRecipes.java
+++ src/main/java/uk/co/nikodem/dFSmpPlus/Crafting/Recipes/CompassRecipes.java
@@ -1,16 +1,19 @@
 package uk.co.nikodem.dFSmpPlus.Crafting.Recipes;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import uk.co.nikodem.dFSmpPlus.Crafting.CraftingTemplate;
+import uk.co.nikodem.dFSmpPlus.Crafting.CustomRecipeMethods.SmithingTable.CustomItemRepresentation;
 import uk.co.nikodem.dFSmpPlus.Crafting.RecipeBuilder.ControlledShapelessRecipeBuilder;
 import uk.co.nikodem.dFSmpPlus.DFSmpPlus;
 import uk.co.nikodem.dFSmpPlus.Items.DFMaterial;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CompassRecipes extends CraftingTemplate {
     public CompassRecipes(DFSmpPlus plugin) {
@@ -29,7 +32,12 @@ public class CompassRecipes extends CraftingTemplate {
     public void doEndRecipe(List<Recipe> recipesToAdd) {
         recipesToAdd.add(
                 new ControlledShapelessRecipeBuilder()
-                        .addIngredient(DFMaterial.LocatorCompass)
+                        .addIngredient(new CustomItemRepresentation(DFMaterial.LocatorCompass, (itemStack) -> {
+                            ItemMeta meta = itemStack.getItemMeta();
+                            if (meta == null) return false;
+                            List<Component> newLore = new ArrayList<>(DFMaterial.LocatorCompass.getLore());
+                            return Objects.equals(meta.lore(), newLore);
+                        }))
                         .addIngredient(DFMaterial.EndLocatorCompassModule)
                         .setResult((r) -> {
                                     ItemStack item = DFMaterial.LocatorCompass.toItemStack();
