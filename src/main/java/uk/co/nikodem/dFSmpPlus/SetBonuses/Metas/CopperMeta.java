45e5151b97714dd555ab2c2642b611e559e2c38a
deadfry42 <pooface442@gmail.com>
2025-11-06 21:52:16 +0000

    only do copper set bonus if weapon used is an actual weapon/tool

--- src/main/java/uk/co/nikodem/dFSmpPlus/SetBonuses/Metas/CopperMeta.java
+++ src/main/java/uk/co/nikodem/dFSmpPlus/SetBonuses/Metas/CopperMeta.java
@@ -11,6 +11,7 @@ import uk.co.nikodem.dFSmpPlus.SetBonuses.DFArmourSetMeta;
 public class CopperMeta implements DFArmourSetMeta {
     @Override
     public void PlayerAttack(Player plr, DFArmourSet armourSet, ItemStack weapon, EntityDamageByEntityEvent event) {
+        if (!DFItemUtils.isTool(weapon)) return;
         if (event.isCritical() && !plr.getWorld().isClearWeather()) {
             int random = DFItemUtils.isCopper(weapon) ? (int) (Math.random() * 10 + 1) : (int) (Math.random() * 25 + 1);
             if (random == 2) {
