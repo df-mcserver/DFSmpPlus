8f173b4da1b58c38674f0905f802c4c1a0970e41
deadfry42 <pooface442@gmail.com>
2025-11-15 19:34:38 +0000

    create gradle build stuff

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+}
+
+group = "uk.co.nikodem"
+version = "2.0.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://nexus.frengor.com/repository/public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    compileOnly("com.frengor:ultimateadvancementapi:2.7.1")
+    implementation("com.google.code.gson:gson:2.13.2")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
