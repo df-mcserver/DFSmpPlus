5b67c277bec2cf1549f46b2cc5f9f60c2d40f86e
deadfry42 <pooface442@gmail.com>
2026-04-01 22:16:14 +0100

    more ender dragon egg prevention
    - detect bundles containing ender dragon eggs
    - don't allow ender dragons in shelves
    - allow ender dragons into specific inventories

--- src/main/java/uk/co/nikodem/dFSmpPlus/Events/Player/PlayerInteractEvent.java
+++ src/main/java/uk/co/nikodem/dFSmpPlus/Events/Player/PlayerInteractEvent.java
@@ -4,12 +4,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import uk.co.nikodem.dFSmpPlus.Accessories.Item.AccessoryEvents;
 import uk.co.nikodem.dFSmpPlus.Items.DFMaterialEvents;
-import uk.co.nikodem.dFSmpPlus.Items.VanillaItems;
+import uk.co.nikodem.dFSmpPlus.Player.EnderEggStoragePreventer;
 import uk.co.nikodem.dFSmpPlus.SetBonuses.SetBonusText;
 
 public class PlayerInteractEvent implements Listener {
     @EventHandler
     public void PlayerInteractEvent(org.bukkit.event.player.PlayerInteractEvent event) {
+        EnderEggStoragePreventer.onBlockPrevention(event);
         SetBonusText.onItemUsed(event);
         AccessoryEvents.UserInteract(event);
         DFMaterialEvents.ItemUse(event);
--- src/main/java/uk/co/nikodem/dFSmpPlus/Player/EnderEggStoragePreventer.java
+++ src/main/java/uk/co/nikodem/dFSmpPlus/Player/EnderEggStoragePreventer.java
@@ -1,34 +1,39 @@
 package uk.co.nikodem.dFSmpPlus.Player;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.Shelf;
 import org.bukkit.entity.GlowItemFrame;
 import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.*;
+import org.bukkit.inventory.meta.BundleMeta;
 
 public class EnderEggStoragePreventer {
     public static void onInventoryClick(InventoryClickEvent event) {
         Inventory clicked = event.getClickedInventory();
+        if (event.getInventory() instanceof AnvilInventory || event.getInventory() instanceof CraftingInventory || event.getInventory() instanceof PlayerInventory) return;
         if (event.getClick().isShiftClick()) {
             if (clicked == event.getWhoClicked().getInventory()) {
-                // The item is being shift clicked from the bottom to the top
                 ItemStack clickedOn = event.getCurrentItem();
 
-                if (clickedOn != null && (clickedOn.getType() == Material.DRAGON_EGG)) {
+                if (clickedOn != null && (isForbiddenItem(clickedOn))) {
                     event.setCancelled(true);
                 }
             }
         } else {
-            if (clicked != event.getWhoClicked().getInventory()) { // Note: !=
+            if (clicked != event.getWhoClicked().getInventory()) {
                 // The cursor item is going into the top inventory
                 ItemStack onCursor = event.getCursor();
 
-                if (onCursor.getType() == Material.DRAGON_EGG){
+                if (isForbiddenItem(onCursor)){
                     event.setCancelled(true);
                 }
             }
@@ -38,7 +43,7 @@ public class EnderEggStoragePreventer {
     public static void onInventoryDrag(InventoryDragEvent event) {
         ItemStack dragged = event.getOldCursor(); // This is the item that is being dragged
 
-        if (dragged.getType() == Material.DRAGON_EGG) {
+        if (isForbiddenItem(dragged)) {
             int inventorySize = event.getInventory().getSize(); // The size of the inventory, for reference
 
             for (int i : event.getRawSlots()) {
@@ -52,15 +57,40 @@ public class EnderEggStoragePreventer {
 
     public static void onInventoryPickupItem(InventoryPickupItemEvent event) {
         if (event.getInventory().getType() == InventoryType.HOPPER) {
-            if (event.getItem().getItemStack().getType() == Material.DRAGON_EGG) event.setCancelled(true);
+            if (isForbiddenItem(event.getItem().getItemStack())) event.setCancelled(true);
+        }
+    }
+
+    public static void onBlockPrevention(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        Block block = event.getClickedBlock();
+        if (block == null) return;
+        if (block.getBlockData() instanceof Shelf) {
+            Player plr = event.getPlayer();
+            if (plr.isSneaking()) return;
+            if (isForbiddenItem(event.getPlayer().getInventory().getItemInMainHand())) {
+                event.setCancelled(true);
+            }
         }
     }
 
     public static void onItemFramePrevention(PlayerInteractEntityEvent event) {
         if (event.getRightClicked() instanceof ItemFrame || event.getRightClicked() instanceof GlowItemFrame) {
-            if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.DRAGON_EGG) {
+            if (isForbiddenItem(event.getPlayer().getInventory().getItemInMainHand())) {
                 event.setCancelled(true);
             }
         }
+    }
+
+    public static boolean isForbiddenItem(ItemStack item) {
+        if (item.getType() == Material.DRAGON_EGG) return true;
+        else if (item.getItemMeta() instanceof BundleMeta bundleMeta) {
+            for (ItemStack bundleitem : bundleMeta.getItems()) {
+                if (bundleitem.getType() == Material.DRAGON_EGG) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
